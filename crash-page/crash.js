3f7246d5752bb4e3017a25d86f56ff436613c6c1
binary333 <69118032+binary333@users.noreply.github.com>
2022-01-15 11:12:03 +0100

    Add files via upload

---
+++ crash-page/crash.js
@@ -0,0 +1,54 @@
+$(".drumClick").on("click",function(){
+    var clickedSound = this.innerHTML;
+    makeSound(clickedSound);
+    animationClick(clickedSound)
+});
+function animationClick(click){
+    var activeButton= document.querySelector("."+ click)
+    activeButton.classList.add("Clicked");
+    setTimeout(() => {
+        activeButton.classList.remove("Clicked");
+    }, 100);
+}
+$(".drumClick").on("keypress", function(evt){
+    makeSound(evt.key);
+    animationClick(evt.key);
+})
+
+
+
+
+
+function makeSound(key){
+    switch (key){
+        case "q":
+            var audioQ = new Audio("./sounds/crash.mp3")
+            audioQ.play();
+        break;
+        case "w":
+            var audioW = new Audio("./sounds/kick-bass.mp3")
+            audioW.play();
+        break
+        case "e":
+            var audioE = new Audio("./sounds/tom-1.mp3")
+            audioE.play();
+        break
+        case "r":
+            var audioR = new Audio("./sounds/tom-2.mp3")
+            audioR.play();
+        break
+        case "t":
+            var audioT = new Audio("./sounds/tom-3.mp3")
+            audioT.play();
+        break
+        case "y":
+            var audioY = new Audio("./sounds/tom-4.mp3")
+            audioY.play();
+        break
+        case "u":
+            var audioU = new Audio("./sounds/crash.mp3")
+            audioU.play();
+        break
+    }
+}
+
